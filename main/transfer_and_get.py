f6510764e53bea017cb4d7849516e22c6d61be44
steven780122 <steven780122@yahoo.com.tw>
2019-04-28 17:15:41 +0800

    update

--- main/transfer_and_get.py
+++ main/transfer_and_get.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-
+data_xls = pd.read_excel('sample/20190424_M5_EURUSD5.xlsx', 'EURUSD5', index_col=None)
 # read_excel('excelfile.xlsx', 'Sheet2', index_col=None)
+print(data_xls)
+
+data_xls.to_csv('csvfile.csv', encoding='utf_8_sig', index=False)
 
