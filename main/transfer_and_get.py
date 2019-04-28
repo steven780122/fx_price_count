1fd95c3955488cc705bd4b23b640684df86c2807
steven780122 <steven780122@yahoo.com.tw>
2019-04-28 22:25:48 +0800

    update read csv

--- main/transfer_and_get.py
+++ main/transfer_and_get.py
@@ -1,8 +1,23 @@
 import pandas as pd
 
-data_xls = pd.read_excel('sample/20190424_M5_EURUSD5.xlsx', 'EURUSD5', index_col=None)
-# read_excel('excelfile.xlsx', 'Sheet2', index_col=None)
-print(data_xls)
+# FILE_INPUT = 'sample/TEST.xlsx'
+FILE_INPUT = 'sample/EURUSD5.csv'
 
-data_xls.to_csv('csvfile.csv', encoding='utf_8_sig', index=False)
+FILE_OUTPUT = 'csvfile.csv'
 
+# data_xls = pd.read_excel('sample/20190424_M5_EURUSD5.xlsx', 'EURUSD5', index_col=None)
+
+# data_xls = pd.read_excel(FILE_INPUT, 'EURUSD5', index_col=None)
+
+
+# data_xls.to_csv(FILE_OUTPUT, encoding='utf_8_sig', index=False)
+
+data_csv_df = pd.read_csv(FILE_INPUT, header=None)    # cause first row is value not head name
+print(data_csv_df)
+print('****1***')
+open_series = data_csv_df.iloc[:, 2]
+high_series = data_csv_df.iloc[:, 3]
+print(open_series)
+print(type(open_series))
+print('****2***')
+print(pd.concat([open_series, high_series], ignore_index=True))
