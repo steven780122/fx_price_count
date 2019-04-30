4b4257f9ddc3376b2f83b6986efefa3760bda3cd
Steven Hsiao <stevenhsiao@starlux-airlines.com>
2019-04-30 08:57:34 +0800

    update

--- main/transfer_and_get.py
+++ main/transfer_and_get.py
@@ -2,10 +2,12 @@ import pandas as pd
 
 # FILE_INPUT = 'sample/TEST.xlsx'
 # FILE_INPUT = 'sample/EURUSD5.csv'
-FILE_INPUT = 'sample/EURUSD5.csv'
+FILE_INPUT = 'sample/EURUSD_5.csv'
 
 
 FILE_OUTPUT = 'csvfile.csv'
+
+
 
 # data_xls = pd.read_excel('sample/20190424_M5_EURUSD5.xlsx', 'EURUSD5', index_col=None)
 # data_xls = pd.read_excel(FILE_INPUT, 'EURUSD5', index_col=None)
@@ -14,6 +16,8 @@ FILE_OUTPUT = 'csvfile.csv'
 data_csv_df = pd.read_csv(FILE_INPUT, header=None)    # cause first row is value not head name
 print(data_csv_df)
 print('****1***')
+
+
 open_series = data_csv_df.iloc[:, 2]
 high_series = data_csv_df.iloc[:, 3]
 low_series = data_csv_df.iloc[:, 4]
@@ -27,8 +31,8 @@ print(concat_series)
 print('****3***')
 price_counts = concat_series.value_counts()
 print(price_counts)
-head_sortbyprice = price_counts.head(50).sort_index(ascending=False)
+head_sortbyprice = price_counts.head(200).sort_index(ascending=False)
 print(head_sortbyprice)    # sort by index (want to list price from low to high)
 
-head_sortbyprice.to_frame().to_csv(FILE_OUTPUT)      # get top high counts's price
 
+head_sortbyprice.to_frame().to_csv(FILE_OUTPUT)      # get top high counts's price
